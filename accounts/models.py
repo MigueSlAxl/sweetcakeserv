1b492182f490524e8c02b4bd04d355710902dadf
Miguel Quiroz <miguel881@live.cl>
2023-05-02 18:28:36 -0400

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -6,37 +6,25 @@ from django.shortcuts import get_object_or_404, redirect
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, default=1)
-    rut = models.CharField(max_length=12, blank=True, null=True)
-    direccion = models.CharField(max_length=255, blank=True, null=True)
-    ntelefono = models.CharField(max_length=15, blank=True, null=True)
-    nemergencia = models.CharField(max_length=15, blank=True, null=True)
-    local = models.CharField(max_length=50, blank=True, null=True)
-
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100,verbose_name='first_name')
+    username = models.CharField(max_length=100,verbose_name='username')
+    last_name = models.CharField(max_length=100,verbose_name='last_name')
+    email = models.CharField(max_length=100,verbose_name='email')
+    password = models.CharField(max_length=100,verbose_name='password')
     class Meta:
         verbose_name = "user"
         verbose_name_plural = "users"
-
     # Info del registro
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha ingreso")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Fecha actualizacion")
-    deleted_at = models.DateTimeField(auto_now=False, verbose_name="Fecha eliminacion", blank=True, null=True)
-
+    deleted_at =  models.DateTimeField(auto_now=False, verbose_name="Fecha eliminacion", blank=True, null=True)
     def __str__(self):
         return self.user.username
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    try:
-        instance.profile.save()
-    except Profile.DoesNotExist:
-        Profile.objects.create(user=instance)
-
+    
+    
+    
+    
+    
+    
+    
